91a8c86914a5bc67da078d118125209462cc6319
illia-dobro <illia.gopro@gmail.com>
2023-11-01 12:51:57 +0200

    + router

--- app.js
+++ app.js
@@ -3,16 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product', (req, res, next) => {
-  console.log(req.body);
-  res.send('<h1>The "Add Product" Page</h1>');
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Hello from Express!</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
